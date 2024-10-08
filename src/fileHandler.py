0a25e56f3208a8a11cf6433cf6c2fd228714a8cd
marianadinis2424 <95451303+marianadinis2424@users.noreply.github.com>
2024-10-08 22:39:56 +0100

    2nd_Update fileHandler.py

--- src/fileHandler.py
+++ src/fileHandler.py
@@ -10,27 +10,28 @@ from src.valid_points import ValidPoints
 class FileHandler:
     """
     The FileHandler ADT is responsible for managing file interactions within the program.
-    It handles reading a specified file containing data about points and generating HTML maps
+    It handles reading a specified file containing data about points and generating HTML maps 
     using those points.
 
     Attributes:
         __filename (str): The name of the file from which point data will be read.
-
+        __points (ValidPoints): The ValidPoints object made of the file points
+        
     Methods:
         __init__(str): Initializes a new FileHandler object with the specified file name.
         The file handler is automatically returned at instantiation.
-
-        read_points(): Reads data about points from the specified file and
+        
+        read_points(): Reads data about points from the specified file and 
         creates a ValidPoints object to store the Point objects. Returns the ValidPoints object.
-
-        make_map(Delimitation): Creates an HTML file containing a map
-        that represents the points stored in the ValidPoints object as circles,
+        
+        make_map(Delimitation): Creates an HTML file containing a map 
+        that represents the points stored in the ValidPoints object as circles, 
         and the provided Delimitation object as a line. Uses the folium module to generate the map.
-
-        __repr__(): Returns a string representation of the FileHandler object, including
+       
+        __repr__(): Returns a string representation of the FileHandler object, including 
         the file name and the points that have been imported.
     """
-
+    
     def __init__(self, filename: str):
         """
         This method initializes a FileHandler object.
@@ -39,9 +40,9 @@ class FileHandler:
 
         Complexity: O(1)
         """
-        self.__filename = filename
+        self.__filename=filename
         self.__points = self.read_points()
-
+    
     def read_points(self) -> ValidPoints:
         """
         This method reads point data from a file and stores it in a ValidPoints object.
@@ -54,11 +55,10 @@ class FileHandler:
         try:
             with open(self.__filename, "r") as md:
                 for line in md:
-                    items = line.strip().split()
-                    if len(items) == 3:
-                        valid_points.append(
-                            Point(items[0], float(items[1]), float(items[2])),
-                        )
+                    items = line.split()
+                    valid_points.append(
+                        Point(items[0], float(items[1]),float(items[2]))
+                    ) 
         except FileNotFoundError:
             raise FileNotFoundError(f"The file '{self.__filename}' was not found.")
         except ValueError as ve:
@@ -67,7 +67,7 @@ class FileHandler:
             print(f"Error reading points from {self.__filename}: {e}")
         return ValidPoints(valid_points)
 
-    def make_map(self, delimitation: Delimitation | None = None):
+    def make_map(self, delimitation = None):
         """
         This method creates a HTML file containing a map that represents the points stored
         in the file handler's ValidPoints object as circles, and the delimitation as
@@ -75,33 +75,46 @@ class FileHandler:
 
         Arguments: delimitation(Delimitation)
 
-        Complexity: O(p+d), p being the number of points of the ValidPoints object and d
+        Complexity: O(plog(p)+d), p being the number of points of the ValidPoints object and d 
         being the number of points of the Delimitation object
         """
-        map = folium.Map()
+        folium_map = folium.Map()
         points = self.__points.get_all_points()
         coords = [(point.get_longitude(), point.get_latitude()) for point in points]
 
+        min_lat = self.__points.get_min_lat()
+        min_lon = self.__points.get_min_lon()
+
+        max_lat = self.__points.get_max_lat()
+        max_lon = self.__points.get_max_lon()
+        
+        folium_map.fit_bounds([[min_lat, min_lon], [max_lat, max_lon]])
+
         try:
             for longitude, latitude in coords:
-                folium.CircleMarker(location=[latitude, longitude], radius=2, weight=5).add_to(map)
-
+                folium.CircleMarker(
+                    location = [latitude, longitude],  
+                    radius = 2,
+                    weight = 5
+                ).add_to(folium_map)
+            
             if delimitation:
-                del_points = delimitation.get_points()
+                del_points = delimitation.get_points()  
                 del_coordinates = [(del_point.get_latitude(), del_point.get_longitude()) for del_point in del_points]
-                folium.PolyLine(locations=del_coordinates, color="blue", weight=2.5).add_to(map)
-
+                folium.PolyLine(locations=del_coordinates, color="blue", weight=2.5).add_to(folium_map)
+                
             self.__filename = f"map_{time.time()}.html"
-            map.save(self.__filename)
+            folium_map.save(self.__filename)
         except Exception as e:
             print(f"Error creating map: {e}")
-
+    
     def __repr__(self) -> str:
         """
         This method returns a representation of the FileHandler object.
 
         Returns: str
-
+        
         Complexity: O(p), p being the number of points in the FileHandler object
         """
-        return f"Filename: {self.__filename}\n Points: {self.read_points()}"
+        return f"Filename: {self.__filename}; {self.__points}"
+    
